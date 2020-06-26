e613f0f1af94dc9357ad35cc6ff9d66fdd7fa945
Rushikesh8421 <patilrushikesh8421@gmail.com>
2020-06-27 00:11:51 +1300

    java oops mini project

---
+++ Java_OOPS/Earth.java
@@ -0,0 +1,18 @@
+package java_OOPS;
+
+public class Earth {
+
+	public static void main(String[] args) {
+		Human human1 = new Human(72, "Guru", 30, "black");
+		Human human2 = new Human(65,"Rushi",19,"black");
+		Human human3 = new Human(60,"Shivani",24,"black");
+		
+		human1.speak();
+		human2.speak();
+		human3.speak();
+		
+
+	}
+
+	
+}
---
+++ Java_OOPS/Human.java
@@ -0,0 +1,23 @@
+package java_OOPS;
+
+public class Human {
+	int heightInInches;
+	String Name;
+	int age;
+	String eyeColor;
+	
+	public Human(int heightInInches, String name, int age, String eyeColor) {
+		super();
+		this.heightInInches = heightInInches;
+		Name = name;
+		this.age = age;
+		this.eyeColor = eyeColor;
+	}
+
+	public void speak() {
+		System.out.println("Hello my name is "+Name);
+		System.out.println("I am "+heightInInches+" inches tall");
+		System.out.println("I am "+age+" years old");
+		System.out.println("My eye color is "+eyeColor);
+	}
+}
