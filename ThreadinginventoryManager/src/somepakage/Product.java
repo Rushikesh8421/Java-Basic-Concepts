c6e951f28c1c79a2ef0d58106107257363efeb2e
Rushikesh8421 <patilrushikesh8421@gmail.com>
2020-07-19 23:36:49 +1300

    added a project on concurrent.CopyOnWriteArrayList

---
+++ ThreadinginventoryManager/src/somepakage/Product.java
@@ -0,0 +1,16 @@
+package somepakage;
+
+public class Product {
+	int id;
+	String name;
+	public Product(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public String toString() {
+		return "id "+id+" | Name "+name;
+		
+	}
+
+}
